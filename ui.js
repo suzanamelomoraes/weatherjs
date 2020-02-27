fe2951bf12a0eb2c7f15787e5837aa91b820bf23
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-27 21:54:36 +1300

    comment detail that has not been used at this moment

--- ui.js
+++ ui.js
@@ -3,7 +3,7 @@ class UI {
     this.location = document.getElementById("w-location");
     this.description = document.getElementById("w-description");
     this.string = document.getElementById("w-string");
-    this.icon = document.getElementById("w-icon");
+    // this.icon = document.getElementById("w-icon");
     this.details = document.getElementById("w-details");
     this.temp = document.getElementById("w-temp");
     this.humidity = document.getElementById("w-humidity");
@@ -15,7 +15,7 @@ class UI {
     this.location.textContent = weather.name;
     this.description.textContent = weather.weather[0].description;
     this.string.textContent = weather.weather[0].main;
-    this.icon.setAttribute("src", weather.weather[0].icon);
+    // this.icon.setAttribute("src", weather.weather[0].icon);
     this.temp.textContent = `Temperature ${weather.main.temp}`;
     this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}`;
     this.feelsLike.textContent = `Feels like: ${weather.main.feels_like}`;

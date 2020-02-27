437f64e272478bbf3e49557887b4dfc2bdd1fa2a
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-27 20:43:07 +1300

    initialize ui

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const weather = new Weather("Miami", "US");
+const ui = new UI();
 
 document.addEventListener("DOMContentLoaded", getWeather);
 
@@ -7,6 +8,7 @@ function getWeather() {
     .getWeather()
     .then(results => {
       ui.display(results);
+      console.log(results);
     })
     .catch(err => console.log(err));
 }

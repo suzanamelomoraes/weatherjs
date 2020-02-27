63b4b4b3f176fdd91fc5706eaa9a85306fd08954
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-27 17:27:54 +1300

    get necessary elements from HTML by ID

---
+++ ui.js
@@ -0,0 +1,13 @@
+class UI {
+  constructor() {
+    this.location = document.getElementById("w-location");
+    this.description = document.getElementById("w-description");
+    this.string = document.getElementById("w-string");
+    this.icon = document.getElementById("w-icon");
+    this.details = document.getElementById("w-details");
+    this.humidity = document.getElementById("w-humidity");
+    this.dewpoint = document.getElementById("w-dewpoint");
+    this.feelsLike = document.getElementById("w-feels-like");
+    this.wind = document.getElementById("w-wind");
+  }
+}

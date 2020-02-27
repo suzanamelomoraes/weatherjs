aadea747f414e6e9587bc5871219ad254599eeee
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-27 21:52:41 +1300

    get change locations button from HTML and call functions to change de location by the input given by the user

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const weather = new Weather("Miami", "US");
+const weather = new Weather("Auckland", "NZ");
 const ui = new UI();
 
 document.addEventListener("DOMContentLoaded", getWeather);
+
+// Change location event
+document.getElementById("w-change-btn").addEventListener("click", e => {
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("state").value;
+
+  weather.changeLocation(city, state);
+
+  getWeather();
+
+  //Close modal
+  $("#locModal").modal("hide");
+});
 
 function getWeather() {
   weather

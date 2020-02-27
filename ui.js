d3500b6d3794b9499e3e09bea0179b5181b2b683
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-27 20:44:05 +1300

    create function to display items fetched from external API

--- ui.js
+++ ui.js
@@ -5,9 +5,20 @@ class UI {
     this.string = document.getElementById("w-string");
     this.icon = document.getElementById("w-icon");
     this.details = document.getElementById("w-details");
+    this.temp = document.getElementById("w-temp");
     this.humidity = document.getElementById("w-humidity");
-    this.dewpoint = document.getElementById("w-dewpoint");
     this.feelsLike = document.getElementById("w-feels-like");
     this.wind = document.getElementById("w-wind");
+  }
+
+  display(weather) {
+    this.location.textContent = weather.name;
+    this.description.textContent = weather.weather[0].description;
+    this.string.textContent = weather.weather[0].main;
+    this.icon.setAttribute("src", weather.weather[0].icon);
+    this.temp.textContent = `Temperature ${weather.main.temp}`;
+    this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}`;
+    this.feelsLike.textContent = `Feels like: ${weather.main.feels_like}`;
+    this.wind.textContent = `Wind: ${weather.wind.speed}`;
   }
 }

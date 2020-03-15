db6bb5085bd7d165104c46473ac5218aef703c23
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-03-16 11:48:04 +1300

    update constructor and display function to display image receveid by the external API

--- ui.js
+++ ui.js
@@ -4,7 +4,7 @@ class UI {
     this.country = document.getElementById("w-country");
     this.description = document.getElementById("w-description");
     this.string = document.getElementById("w-string");
-    // this.icon = document.getElementById("w-icon");
+    this.icon = document.getElementById("w-icon");
     this.details = document.getElementById("w-details");
     this.temp = document.getElementById("w-temp");
     this.humidity = document.getElementById("w-humidity");
@@ -17,8 +17,8 @@ class UI {
     this.country.textContent = weather.sys.country;
     this.description.textContent = `Description: ${weather.weather[0].description}`;
     this.string.textContent = weather.weather[0].main;
-    // this.icon.setAttribute("src", weather.weather[0].icon);
-    this.temp.textContent = `Temperature ${weather.main.temp}`;
+    this.icon.setAttribute("src", `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+    this.temp.textContent = `Temperature: ${weather.main.temp}`;
     this.humidity.textContent = `Relative Humidity: ${weather.main.humidity} %`;
     this.feelsLike.textContent = `Feels like: ${weather.main.feels_like}`;
     this.wind.textContent = `Wind: ${weather.wind.speed} km/h`;

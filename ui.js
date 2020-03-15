ead5cbba986644e9efbd717a10c1640f15ed7485
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-03-16 12:04:04 +1300

    cleanning and organising code

--- ui.js
+++ ui.js
@@ -2,11 +2,10 @@ class UI {
   constructor() {
     this.location = document.getElementById("w-location");
     this.country = document.getElementById("w-country");
-    this.description = document.getElementById("w-description");
-    this.string = document.getElementById("w-string");
-    this.icon = document.getElementById("w-icon");
-    this.details = document.getElementById("w-details");
+    this.string = document.getElementById("w-main-weather");
     this.temp = document.getElementById("w-temp");
+    this.icon = document.getElementById("w-icon");
+    this.description = document.getElementById("w-description");
     this.humidity = document.getElementById("w-humidity");
     this.feelsLike = document.getElementById("w-feels-like");
     this.wind = document.getElementById("w-wind");
@@ -15,10 +14,10 @@ class UI {
   display(weather) {
     this.location.textContent = weather.name;
     this.country.textContent = weather.sys.country;
-    this.description.textContent = `Description: ${weather.weather[0].description}`;
     this.string.textContent = weather.weather[0].main;
-    this.icon.setAttribute("src", `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
     this.temp.textContent = `Temperature: ${weather.main.temp} C`;
+    this.icon.setAttribute("src", `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+    this.description.textContent = `Description: ${weather.weather[0].description}`;
     this.humidity.textContent = `Relative Humidity: ${weather.main.humidity} %`;
     this.feelsLike.textContent = `Feels like: ${weather.main.feels_like} C`;
     this.wind.textContent = `Wind: ${weather.wind.speed} km/h`;
